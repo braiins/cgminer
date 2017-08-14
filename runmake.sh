49ea78192e141368fec1f36813db00bd52ef4aa9
PengPeng <516190948@qq.com>
2017-08-14 13:58:52 +0800

    将并行编译数目参数化,避免-j16卡死服务器

--- runmake.sh
+++ runmake.sh
@@ -4,11 +4,12 @@
 ./autogen.sh
 
 ROOTFS_DIR=$1
+MAKE_JOBS=$2
 
 LDFLAGS=-L${ROOTFS_DIR}/lib \
 CFLAGS=-I${ROOTFS_DIR}/include \
 ./configure --prefix=${ROOTFS_DIR} \
 --enable-bitmine_A1 --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
 
-make -j16
+make -j${MAKE_JOBS}
 
