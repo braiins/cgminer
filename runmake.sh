520891b15c8d823434b602448ada91e5da16cbb9
PengPeng <516190948@qq.com>
2017-08-14 13:58:52 +0800

    完善并行编译数目为-j4,避免-j16卡死服务器

--- runmake.sh
+++ runmake.sh
@@ -10,5 +10,5 @@ CFLAGS=-I${ROOTFS_DIR}/include \
 ./configure --prefix=${ROOTFS_DIR} \
 --enable-bitmine_A1 --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
 
-make -j16
+make -j4
 
