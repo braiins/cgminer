36403731c63b2a3bc3453c491ab60e0c12edc1b9
Jan Čapek <jan.capek@braiins.cz>
2017-11-24 10:14:43 +0100

    Add DEBUG flag support
    - append DEBUG_CFLAGS to CFLAGS

--- runmake.sh
+++ runmake.sh
@@ -5,13 +5,14 @@
 
 ROOTFS_DIR=$1
 MAKE_JOBS=$2
+DEBUG_CFLAGS=$3
 
 # A5
 CHIP_TYPE=A5
 sed -i "s/#define CHIP_A[0-9]/#define CHIP_A5/g" miner.h
 ./autogen.sh
 LDFLAGS=-L${ROOTFS_DIR}/lib \
-CFLAGS=-I${ROOTFS_DIR}/include \
+CFLAGS="-I${ROOTFS_DIR}/include ${DEBUG_CFLAGS}" \
 ./configure --prefix=${ROOTFS_DIR} \
 --enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
 make -j${MAKE_JOBS}
