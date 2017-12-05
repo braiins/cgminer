a0d021b321cc1accd6a001cb9f58f9b803a15678
Jan Čapek <jan.capek@braiins.cz>
2017-12-05 07:50:20 +0100

    Simplify runmake.sh
    - remove copy/pasted configuration/compilation snippet
    - make install part of the build process - the compilation generates 2
      executables - braiins-a5-cgminer and braiins-a6-cgminer

--- runinstall.sh
+++ runinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# NOTE: This script is not needed anymore, installation is done in
+# 'runmake.sh'. It is retained not to break external compilation
+# workflow
 ROOTFS_DIR=$1
-
-make DESTDIR=${ROOTFS_DIR} install
--- runmake.sh
+++ runmake.sh
@@ -7,27 +7,14 @@ ROOTFS_DIR=$1
 MAKE_JOBS=$2
 DEBUG_CFLAGS=$3
 
-# A5
-CHIP_TYPE=A5
-sed -i "s/#define CHIP_A[0-9]/#define CHIP_A5/g" miner.h
-./autogen.sh
-LDFLAGS=-L${ROOTFS_DIR}/lib \
-CFLAGS="-I${ROOTFS_DIR}/include ${DEBUG_CFLAGS}" \
-       ./configure --prefix=/ \
-       --program-prefix=braiins- \
-       --enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
-make -j${MAKE_JOBS}
-cp cgminer innominer_T1
-
-# A6
-CHIP_TYPE=A6
-sed -i "s/#define CHIP_A[0-9]/#define CHIP_A6/g" miner.h
-./autogen.sh
-LDFLAGS=-L${ROOTFS_DIR}/lib \
-CFLAGS=-I${ROOTFS_DIR}/include \
-./configure --prefix=${ROOTFS_DIR} \
---enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
-make -j${MAKE_JOBS}
-cp cgminer innominer_T2
-
-
+for i in 5 6; do
+    CHIP_TYPE=A$i
+    sed -i "s/#define CHIP_A[0-9]/#define CHIP_A$i/g" miner.h
+    ./autogen.sh
+    LDFLAGS=-L${ROOTFS_DIR}/lib \
+	   CFLAGS="-I${ROOTFS_DIR}/include ${DEBUG_CFLAGS}" \
+	   ./configure --prefix=/ --program-prefix=braiins-a$i \
+	   --enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
+    make -j${MAKE_JOBS}
+    make DESTDIR=${ROOTFS_DIR} install
+done
