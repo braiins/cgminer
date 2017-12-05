93f3679c8179d32472add42e517fe3dbcf1e3e88
Libor Vašíček <libor.vasicek@braiins.cz>
2017-12-05 14:23:42 +0100

    Fix missing flags for libcurl
    - compile cgminer with target specific libcurl instead of host
      system library
    - replace tabs with spaces

--- runmake.sh
+++ runmake.sh
@@ -12,9 +12,11 @@ for i in 5 6; do
     sed -i "s/#define CHIP_A[0-9]/#define CHIP_A$i/g" miner.h
     ./autogen.sh
     LDFLAGS=-L${ROOTFS_DIR}/lib \
-	   CFLAGS="-I${ROOTFS_DIR}/include ${DEBUG_CFLAGS}" \
-	   ./configure --prefix=/ --program-prefix=braiins-a$i \
-	   --enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
+        CFLAGS="-I${ROOTFS_DIR}/include ${DEBUG_CFLAGS}" \
+        LIBCURL_CFLAGS="-I${ROOTFS_DIR}/include/curl" \
+        LIBCURL_LIBS="-L${ROOTFS_DIR}/lib -lcurl" \
+        ./configure --prefix=/ --program-prefix=braiins-a$i \
+        --enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
     make -j${MAKE_JOBS}
     make DESTDIR=${ROOTFS_DIR} install
 done
