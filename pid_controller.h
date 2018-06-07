06339fec79c9eeecbb2afc005458221c0a6f8767
Jakub Horak <jakub.horak@braiins.cz>
2018-06-07 09:56:31 +0200

    Remove trailing spaces

--- pid_controller.h
+++ pid_controller.h
@@ -1,41 +1,41 @@
 //*********************************************************************************
 // Arduino PID Library Version 1.0.1 Modified Version for C -
 // Platform Independent
-// 
+//
 // Revision: 1.1
-// 
+//
 // Description: The PID Controller module originally meant for Arduino made
 // platform independent. Some small bugs present in the original Arduino source
 // have been rectified as well.
-// 
+//
 // For a detailed explanation of the theory behind this library, go to:
 // http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
-// 
+//
 // Revisions can be found here:
 // https://github.com/tcleg
-// 
+//
 // Modified by: Trent Cleghorn , <trentoncleghorn@gmail.com>
-// 
+//
 // Copyright (C) Brett Beauregard , <br3ttb@gmail.com>
-// 
+//
 //                                 GPLv3 License
-// 
-// This program is free software: you can redistribute it and/or modify it under 
-// the terms of the GNU General Public License as published by the Free Software 
-// Foundation, either version 3 of the License, or (at your option) any later 
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
 // version.
-// 
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License along with 
+//
+// You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
 //*********************************************************************************
 
-// 
+//
 // Header Guard
-// 
+//
 #ifndef PID_CONTROLLER_H
 #define PID_CONTROLLER_H
 
@@ -45,9 +45,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// 
+//
 // C Binding for C++ Compilers
-// 
+//
 #ifdef __cplusplus
 extern "C"
 {
@@ -73,66 +73,66 @@ PIDDirection;
 
 typedef struct
 {
-    // 
+    //
     // Input to the PID Controller
-    // 
+    //
     float input;
-    
-    // 
+
+    //
     // Previous input to the PID Controller
-    // 
+    //
     float lastInput;
-    
-    // 
+
+    //
     // Output of the PID Controller
-    // 
+    //
     float output;
-    
-    // 
+
+    //
     // Gain constant values that were passed by the user
     // These are for display purposes
-    // 
+    //
     float dispKp;
     float dispKi;
     float dispKd;
-    
-    // 
+
+    //
     // Gain constant values that the controller alters for
     // its own use
-    // 
+    //
     float alteredKp;
     float alteredKi;
     float alteredKd;
-    
-    // 
+
+    //
     // The Integral Term
-    // 
+    //
     float iTerm;
-    
-    // 
+
+    //
     // The values that the output will be constrained to
-    // 
+    //
     float outMin;
     float outMax;
-    
-    // 
+
+    //
     // The user chosen operating point
-    // 
+    //
     float setpoint;
-    
-    // 
+
+    //
     // The sense of direction of the controller
     // DIRECT:  A positive setpoint gives a positive output
     // REVERSE: A positive setpoint gives a negative output
-    // 
+    //
     PIDDirection controllerDirection;
-    
-    // 
+
+    //
     // Tells how the controller should respond if the user has
     // taken over manual control or not
     // MANUAL:    PID controller is off.
     // AUTOMATIC: PID controller is on.
-    // 
+    //
     PIDMode mode;
 }
 PIDControl;
@@ -141,7 +141,7 @@ PIDControl;
 // Prototypes
 //*********************************************************************************
 
-// 
+//
 // PID Initialize
 // Description:
 //      Initializes a PIDControl instantiation. This should be called at least once
@@ -155,7 +155,7 @@ PIDControl;
 //      maxOutput - Constrain PID output to this maximum value.
 //      mode - Tells how the controller should respond if the user has taken over
 //             manual control or not.
-//             MANUAL:    PID controller is off. User can manually control the 
+//             MANUAL:    PID controller is off. User can manually control the
 //                        output.
 //             AUTOMATIC: PID controller is on. PID controller controls the output.
 //      controllerDirection - The sense of direction of the controller
@@ -163,12 +163,12 @@ PIDControl;
 //                            REVERSE: A positive setpoint gives a negative output.
 // Returns:
 //      Nothing.
-// 
+//
 extern void PIDInit(PIDControl *pid, float kp, float ki, float kd,
                     float minOutput, float maxOutput,
                     PIDMode mode, PIDDirection controllerDirection);
 
-// 
+//
 // PID Compute
 // Description:
 //      Typically, PIDSetpointSet and PIDInputSet should be called immediately
@@ -178,10 +178,10 @@ extern void PIDInit(PIDControl *pid, float kp, float ki, float kd,
 //	dt - time in seconds since last call
 // Returns:
 //      True if in AUTOMATIC. False if in MANUAL.
-//                     
+//
 extern bool PIDCompute(PIDControl *pid, float dt);
 
-// 
+//
 // PID Mode Set
 // Description:
 //      Sets the PID controller to a new mode.
@@ -189,15 +189,15 @@ extern bool PIDCompute(PIDControl *pid, float dt);
 //      pid - The address of a PIDControl instantiation.
 //      mode - Tells how the controller should respond if the user has taken over
 //             manual control or not.
-//             MANUAL:    PID controller is off. User can manually control the 
+//             MANUAL:    PID controller is off. User can manually control the
 //                        output.
 //             AUTOMATIC: PID controller is on. PID controller controls the output.
 // Returns:
 //      Nothing.
-//              
-extern void PIDModeSet(PIDControl *pid, PIDMode mode);                                                                                                                                       
+//
+extern void PIDModeSet(PIDControl *pid, PIDMode mode);
 
-// 
+//
 // PID Output Limits Set
 // Description:
 //      Sets the new output limits. The new limits are applied to the PID
@@ -208,10 +208,10 @@ extern void PIDModeSet(PIDControl *pid, PIDMode mode);
 //      max - Constrain PID output to this maximum value.
 // Returns:
 //      Nothing.
-// 
-extern void PIDOutputLimitsSet(PIDControl *pid, float min, float max); 							  							  
+//
+extern void PIDOutputLimitsSet(PIDControl *pid, float min, float max);
 
-// 
+//
 // PID Tunings Set
 // Description:
 //      Sets the new gain constant values.
@@ -222,10 +222,10 @@ extern void PIDOutputLimitsSet(PIDControl *pid, float min, float max);
 //      kd - Positive D gain constant value.
 // Returns:
 //      Nothing.
-// 
-extern void PIDTuningsSet(PIDControl *pid, float kp, float ki, float kd);         	                                         
+//
+extern void PIDTuningsSet(PIDControl *pid, float kp, float ki, float kd);
 
-// 
+//
 // PID Tuning Gain Constant P Set
 // Description:
 //      Sets the proportional gain constant value.
@@ -234,10 +234,10 @@ extern void PIDTuningsSet(PIDControl *pid, float kp, float ki, float kd);
 //      kp - Positive P gain constant value.
 // Returns:
 //      Nothing.
-// 
+//
 extern void PIDTuningKpSet(PIDControl *pid, float kp);
 
-// 
+//
 // PID Tuning Gain Constant I Set
 // Description:
 //      Sets the proportional gain constant value.
@@ -246,10 +246,10 @@ extern void PIDTuningKpSet(PIDControl *pid, float kp);
 //      ki - Positive I gain constant value.
 // Returns:
 //      Nothing.
-// 
+//
 extern void PIDTuningKiSet(PIDControl *pid, float ki);
 
-// 
+//
 // PID Tuning Gain Constant D Set
 // Description:
 //      Sets the proportional gain constant value.
@@ -258,14 +258,14 @@ extern void PIDTuningKiSet(PIDControl *pid, float ki);
 //      kd - Positive D gain constant value.
 // Returns:
 //      Nothing.
-// 
+//
 extern void PIDTuningKdSet(PIDControl *pid, float kd);
 
-// 
+//
 // Basic Set and Get Functions for PID Parameters
-// 
+//
 
-// 
+//
 // PID Setpoint Set
 // Description:
 //      Alters the setpoint the PID controller will try to achieve.
@@ -274,11 +274,11 @@ extern void PIDTuningKdSet(PIDControl *pid, float kd);
 //      setpoint - The desired setpoint the PID controller will try to obtain.
 // Returns:
 //      Nothing.
-// 
-static inline void 
+//
+static inline void
 PIDSetpointSet(PIDControl *pid, float setpoint) { pid->setpoint = setpoint; }
 
-// 
+//
 // PID Input Set
 // Description:
 //      Should be called before calling PIDCompute so the PID controller will
@@ -288,11 +288,11 @@ PIDSetpointSet(PIDControl *pid, float setpoint) { pid->setpoint = setpoint; }
 //      input - The value the controller will work with.
 // Returns:
 //      Nothing.
-// 
-static inline void 
+//
+static inline void
 PIDInputSet(PIDControl *pid, float input) { pid->input = input; }
 
-// 
+//
 // PID Output Get
 // Description:
 //      Typically, this function is called after PIDCompute in order to
@@ -301,11 +301,11 @@ PIDInputSet(PIDControl *pid, float input) { pid->input = input; }
 //      pid - The address of a PIDControl instantiation.
 // Returns:
 //      The output of the specific PID controller.
-// 
-static inline float 
+//
+static inline float
 PIDOutputGet(PIDControl *pid) { return pid->output; }
 
-// 
+//
 // PID Proportional Gain Constant Get
 // Description:
 //      Returns the proportional gain constant value the particular
@@ -314,11 +314,11 @@ PIDOutputGet(PIDControl *pid) { return pid->output; }
 //      pid - The address of a PIDControl instantiation.
 // Returns:
 //      The proportional gain constant.
-// 
-inline float 
-PIDKpGet(PIDControl *pid) { return pid->dispKp; }						  
+//
+inline float
+PIDKpGet(PIDControl *pid) { return pid->dispKp; }
 
-// 
+//
 // PID Integral Gain Constant Get
 // Description:
 //      Returns the integral gain constant value the particular
@@ -327,11 +327,11 @@ PIDKpGet(PIDControl *pid) { return pid->dispKp; }
 //      pid - The address of a PIDControl instantiation.
 // Returns:
 //      The integral gain constant.
-// 
-inline float 
-PIDKiGet(PIDControl *pid) { return pid->dispKi; }						  
+//
+inline float
+PIDKiGet(PIDControl *pid) { return pid->dispKi; }
 
-// 
+//
 // PID Derivative Gain Constant Get
 // Description:
 //      Returns the derivative gain constant value the particular
@@ -340,24 +340,24 @@ PIDKiGet(PIDControl *pid) { return pid->dispKi; }
 //      pid - The address of a PIDControl instantiation.
 // Returns:
 //      The derivative gain constant.
-// 
-inline float 
-PIDKdGet(PIDControl *pid) { return pid->dispKd; }						  
+//
+inline float
+PIDKdGet(PIDControl *pid) { return pid->dispKd; }
 
-// 
+//
 // PID Mode Get
 // Description:
 //      Returns the mode the particular controller is set to.
 // Parameters:
 //      pid - The address of a PIDControl instantiation.
 // Returns:
-//      MANUAL or AUTOMATIC depending on what the user set the 
+//      MANUAL or AUTOMATIC depending on what the user set the
 //      controller to.
-// 
-inline PIDMode 
-PIDModeGet(PIDControl *pid) { return pid->mode; }						  
+//
+inline PIDMode
+PIDModeGet(PIDControl *pid) { return pid->mode; }
 
-// 
+//
 // PID Direction Get
 // Description:
 //      Returns the direction the particular controller is set to.
@@ -366,14 +366,14 @@ PIDModeGet(PIDControl *pid) { return pid->mode; }
 // Returns:
 //      DIRECT or REVERSE depending on what the user set the
 //      controller to.
-// 
-inline PIDDirection 
-PIDDirectionGet(PIDControl *pid) { return pid->controllerDirection; }		
+//
+inline PIDDirection
+PIDDirectionGet(PIDControl *pid) { return pid->controllerDirection; }
 
 
-// 
+//
 // End of C Binding
-// 
+//
 #ifdef __cplusplus
 }
 #endif
